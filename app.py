a7a1bde36ea4fd3dd94419fd78601f07949aad7c
ChaitanyaChannella <channellac@gmail.com>
2015-08-21 10:31:13 +0100

    using lists

--- app.py
+++ app.py
@@ -31,7 +31,15 @@ def login_required(f):
 def home():
     g.db = connect_db()
     cur = g.db.execute('select * from posts')
-    posts = [dict(title=row[0],description=row[1]) for row in cur.fetchall()]
+    posts = []
+
+    for row in cur.fetchall():
+
+      posts.append(dict(title=row[0],description=row[1]))
+
+
+    print "\nfor loop:\n{}\n".format(posts)
+
     g.db.close()
     return render_template('index.html',posts=posts)  # render a template
     # return "Hello, World!"  # return a string
